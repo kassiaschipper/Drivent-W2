02a5a316c8ea38a3567d0e8ee2a34b52192edb82
Kássia <kassia_guterres@outlook.com>
2022-11-18 12:09:00 -0300

    implemented findManyTickets whith prisma

--- src/repositories/tickets-repository/index.ts
+++ src/repositories/tickets-repository/index.ts
@@ -1,12 +1,21 @@
 import { prisma } from "@/config";
 
 async function findMany() {
-  const tesste = prisma.ticketType.findMany();
-  return tesste; 
+  return prisma.ticketType.findMany(); 
+}
+
+async function findManyTickets() {
+  return prisma.ticket.findMany({
+    include: {
+      TicketType: true,
+    }
+
+  });
 }
 
 const ticketsRepository = {
   findMany,
+  findManyTickets
 };
 
 export default ticketsRepository;
